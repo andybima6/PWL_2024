52f8ef1be2ef0ae6ade151520eb196f21cfb0db4
Andy Nugraha Putra <andybima6@gmail.com>
2024-02-20 09:44:52 +0700

    penambahan route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
